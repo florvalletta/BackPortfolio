38d30fb9d09e65243dd6b1c70a017e4872d6653d
Maria Florencia Valletta <mflorenciavalletta@gmail.com>
2022-05-24 19:00:33 -0300

    .

--- Back/abml_/src/main/java/com/crud/abml_/controller/AcercaDeController.java
+++ Back/abml_/src/main/java/com/crud/abml_/controller/AcercaDeController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acercaDe")
-@CrossOrigin(origins = "https://florenciavportfolio.web.app/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AcercaDeController {
     
     @Autowired
